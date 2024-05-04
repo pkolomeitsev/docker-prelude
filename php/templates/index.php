b991879b21c63e03db3ebd0e79cb69dd1a873e72
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-05-04 22:58:18 +0300

    added PHP version to index.php template

--- php/templates/index.php
+++ php/templates/index.php
@@ -1,5 +1,6 @@
 <html lang="en">
 <title><site_name></title>
 <h1>Welcome to the <site_name> with NGiNX server!</h1>
-<?php echo 'Project directory (in Docker): ' . __DIR__; ?>
+<p><?php echo 'Project directory (in Docker): ' . __DIR__; ?></p>
+<p>PHP <?php echo phpversion(); ?></p>
 </html>
