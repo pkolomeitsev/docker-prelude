ec4dacf405aec61063d783d70184b914e87c380e
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-27 15:14:22 +0300

    implemented Vue HomePage.vue

---
+++ apps/default/ui/admin-app/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Navigation from './blocks/NavBlock.vue'
+import Hero from './blocks/HeroBlock.vue'
+import Info from './blocks/InfoBlock.vue'
+import Footer from './blocks/FooterBlock.vue'
+
+const props = defineProps(['appConfig'])
+console.log(props.appConfig)
+</script>
+
+<template>
+  <div class="gradient">
+    <Navigation :appName="appConfig.appName" :gitHubLink="appConfig.gitHubLink" />
+    <Hero />
+  </div>
+  <Info :systemInfo="appConfig.systemInfo" />
+  <Footer />
+</template>
