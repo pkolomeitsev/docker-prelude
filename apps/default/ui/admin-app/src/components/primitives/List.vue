5e69f67ef9f5c9fde253fa984dc7628e0e89cd2f
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-27 15:13:48 +0300

    implemented Vue components Primitives

---
+++ apps/default/ui/admin-app/src/components/primitives/List.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ items: Object }>()
+// console.log(items)
+</script>
+
+<template>
+  <ul>
+    <li v-for="(value, key) in items" :key="key">
+      {{
+        key.charAt(0).toUpperCase() +
+        key
+          .split(/\.?(?=[A-Z])/)
+          .join(' ')
+          .slice(1)
+      }}: {{ value }}
+    </li>
+  </ul>
+</template>
