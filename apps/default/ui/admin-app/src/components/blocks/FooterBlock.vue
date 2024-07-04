04fe484e648f019ada8eac3e7cec20499d26d14e
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-07-04 17:26:07 +0300

    FooterBlock refactoring

--- apps/default/ui/admin-app/src/components/blocks/FooterBlock.vue
+++ apps/default/ui/admin-app/src/components/blocks/FooterBlock.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const date = ref(new Date(Date.now()).getFullYear())
+</script>
 <template>
   <!--Footer-->
   <footer class="mt-auto border-t bg-gray-100 py-4">
     <div class="container mx-auto w-full px-4">
-      &copy; 2024 All rights reserved - your localhost
+      &copy; {{ date }} All rights reserved - your localhost
     </div>
   </footer>
 </template>
