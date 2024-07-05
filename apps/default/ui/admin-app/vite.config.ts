e53bad62420b78de10e45fe64de6c29cab3e0e45
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-07-06 00:28:36 +0300

    installed vite-plugin-package-version

--- apps/default/ui/admin-app/vite.config.ts
+++ apps/default/ui/admin-app/vite.config.ts
@@ -1,24 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import version from 'vite-plugin-package-version'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx(), version()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
-    outDir: '../../public',
+    outDir: '../../public'
   }
 })
